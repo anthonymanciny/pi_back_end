c7284f6690d481c9e12af825fa02b9f235438fdc
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-11-08 20:36:04 -0400

    Update app-router.ts

--- src/routes/app-router.ts
+++ src/routes/app-router.ts
@@ -7,26 +7,18 @@ export class AppRouter {
     
     private app_express: any;
 
-    // private bancoRouter: BancoRouter;
-    // private agenciaRouter: AgenciaRouter;
-    // private contaRouter: ContaRouter;
     private usuarioRouter:UsuarioRouter
     
     constructor(app_server: any) {
         this.app_express = app_server;
 
         //criar as rotas
-        // this.bancoRouter = new BancoRouter();
-        // this.agenciaRouter = new AgenciaRouter();
-        // this.contaRouter = new ContaRouter();
+     
         this.usuarioRouter = new UsuarioRouter();
 
     }
-
     public carregarRotas() {
-        // this.app_express.use('/api/banco', this.bancoRouter.router);
-        // this.app_express.use('/api/agencia', this.agenciaRouter.router);
-        // this.app_express.use('/api/conta', this.contaRouter.router);
+
         this.app_express.use('/api/usuario', this.usuarioRouter.router);
     }
 
